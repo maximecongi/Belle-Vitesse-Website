c1d5ab9a498560a23b09423190e2fd2a79be0beb
maximecongi <maxime.congi@live.fr>
2025-12-30 19:49:25 +0100

    Minor Layout Changes
    Changes in the "vehicule" template

--- static/js/src/slider.js
+++ static/js/src/slider.js
@@ -1,9 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
     var splide = new Splide('.splide', {
         type: 'loop',
-        perPage: 2,
-        focus  : 0,
-        omitEnd: true,
     });
 
     splide.mount();
